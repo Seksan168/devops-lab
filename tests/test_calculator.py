c51c55808319568a6eba4f3f16a5f6f93a9a20f9
Seksan Jeennamsai <seksan@Seksans-MacBook-Pro.local>
2025-04-04 14:36:01 +0700

    fix error

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,20 +1,23 @@
-# tests/test_calculator.py
 
-import pytest
-from app import calculator
 
-def test_add():
-    assert calculator.add(3, 2) == 5
+import unittest
+from app.calculator import add, subtract,divide , multiply
 
-def test_subtract():
-    assert calculator.subtract(5, 2) == 3
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(3, 2), 5)
 
-def test_multiply():
-    assert calculator.multiply(4, 3) == 12
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 4), 6)
 
-def test_divide():
-    assert calculator.divide(10, 2) == 5
+    def test_multiply(self):
+        self.assertEqual(multiply(3, 7), 21)
 
-def test_divide_by_zero():
-    with pytest.raises(ValueError, match="Cannot divide by zero."):
-        calculator.divide(5, 0)
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            divide(10, 0)
+if __name__ == '__main__':
+    unittest.main()
